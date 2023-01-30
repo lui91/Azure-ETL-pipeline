9e8dabc34bc5a6c5e52068fdb25223e0fafd1abb
luis ramirez <luis.ramirezsolis@cinvestav.mx>
2023-01-30 16:56:58 -0700

    File sensors implemented

---
+++ py_process/process.py
@@ -0,0 +1,12 @@
+
+
+def print_params(**kwargs):
+    '''
+    dict_keys(['conf', 'dag', 'dag_run', 'data_interval_end', 'data_interval_start', 'ds', 'ds_nodash', 'execution_date',
+    'expanded_ti_count', 'inlets', 'logical_date', 'macros', 'next_ds', 'next_ds_nodash', 'next_execution_date', 'outlets',
+     'params', 'prev_data_interval_start_success', 'prev_data_interval_end_success', 'prev_ds', 'prev_ds_nodash', 
+     'prev_execution_date', 'prev_execution_date_success', 'prev_start_date_success', 'run_id', 'task', 'task_instance', 
+     'task_instance_key_str', 'test_mode', 'ti', 'tomorrow_ds', 'tomorrow_ds_nodash', 'triggering_dataset_events', 'ts', 
+     'ts_nodash', 'ts_nodash_with_tz', 'var', 'conn', 'yesterday_ds', 'yesterday_ds_nodash', 'templates_dict'])
+    '''
+    print(f"Dag: {kwargs['dag']}| Dag run: {kwargs['dag_run']}| Task: {kwargs['task']}| ")
